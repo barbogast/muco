4fd546f5a9db23395d0b9bdc70f94ef0f0e9cfb4
koblaid <koblaid@googlemail.com>
2010-08-30 21:00:34 +0000

    Implemented apsw foreign key checks
    git-svn-id: svn://localhost/media/d70c7252-e701-4b13-91de-3966ca887514/home/ben/Downloads/repos/muco/trunk@6 e26096cf-d81d-4f4f-8e0a-38ab98bea2f3

--- muco/test/test.py
+++ muco/test/test.py
@@ -4,38 +4,54 @@ import unittest as ut
 import sqlite3
 import os
 
-import sys,pprint
-pprint.pprint(sys.path)
+import apsw
+
+
 from muco.model import Model
-
-
 
 
 class SqliteForeignKeys(ut.TestCase):
     TEST_DB_PATH = os.path.join(os.path.realpath('./testdata'), 'test.sqlite')
     
     def setUp(self):
-        print os.listdir('.')
-        self.conn = sqlite3.connect(self.TEST_DB_PATH)
-        self.model = Model()
-        self.model.set_connection(self.conn)
-        self.model.make_schema()
+        try: os.remove(self.TEST_DB_PATH) 
+        except: pass
+        self.conn = apsw.Connection(self.TEST_DB_PATH)
+        self.conn.cursor().execute(open('../schema.sql').read())
+        cur = self.conn.cursor()
+        cur.execute("insert into folder (name) values ('folder1')")
+        cur.execute("""insert into file (name, folder_id) values ('file1', 
+                            (select id from folder where name = 'folder1'))""")
 
     def tearDown(self):
+        #self.conn.commit()
         self.conn.close()
         os.remove(self.TEST_DB_PATH)
         
     def test_select(self):
-        pass
+        cur = self.conn.cursor()
+        cur.execute("select fo.name from folder fo, file fi where fi.folder_id = fo.id")
+        res = list(cur)[0][0]
+        self.assertEqual(res, 'folder1')
         
     def test_insert(self):
-        pass
+        def doIt(cur):
+            cur.execute("insert into file (name, folder_id) values ('file2', 2)")
+        cur = self.conn.cursor()
+        self.assertRaises(apsw.ConstraintError, doIt, cur)
+        self.assertRaises
     
     def test_update(self):
-        pass
+        def doIt(cur):
+            cur.execute("update file set folder_id = 3 where id = 1")
+        cur = self.conn.cursor()
+        self.assertRaises(apsw.ConstraintError, doIt, cur)
 
     def test_delete(self):
-        pass
+        def doIt(cur):
+            cur.execute("delete from folder where id = 1")
+        cur = self.conn.cursor()
+        self.assertRaises(apsw.ConstraintError, doIt, cur)
     
     
 class TestModel(ut.TestCase):
